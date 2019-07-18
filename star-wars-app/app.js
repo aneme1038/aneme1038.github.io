f578dd53b92fe35d83ea2471a864228d33073c43
aneme1038 <thriftman25@gmail.com>
2019-07-18 14:32:31 -0400

    AJAX setup and linked

--- star-wars-app/app.js
+++ star-wars-app/app.js
@@ -1,1 +1,35 @@
-console.log("hi");
+///////////////////
+//AJAX SETUP
+//////////////////
+$('#questionButton').on('click', (event) => {
+  $.ajax({
+    url: 'https://swapi.co/api/films/',
+    type: "GET"
+  }).then( (data) => {
+    console.log("Retrieved " + data.length + " records from the dataset!");
+    console.log(data);
+    films.push(data.results);
+    console.log(data.results);
+  },
+  (error) => {
+    console.log(error);
+  });
+})
+
+/////////////////////////////
+//CODE TO CONSIDER LATER
+////////////////////////////
+
+//BELOW CODE IS FOR SUBMITTING THE ANSWER FORM SECTION. CONSIDER USING.
+/*var form = document.querySelector("form");
+var log = document.querySelector("#log");
+
+form.addEventListener("submit", function(event) {
+  var data = new FormData(form);
+  var output = "";
+  for (const entry of data) {
+    output = entry[0] + "=" + entry[1] + "\r";
+  };
+  log.innerText = output;
+  event.preventDefault();
+}, false);*/
