0a02eab028397ef7259561729218bdf0041f9c9d
aneme1038 <thriftman25@gmail.com>
2019-09-26 21:54:47 -0400

    added users.js controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,27 @@
 const express = require('express');
+const router = express.Router();
+const User = require('../models/users.js');
+const Form = require('../modells/forms.js');
+const bcrypt = require('bcrypt');
+
+//-----------
+//routes
+//---------
+router.get('/new', (req, res) => {
+  res.render('users/admin.html');
+});
+//POST
+router.post('/', (req, res) => {
+  //overwirte the user password with a hashed password then pass it into database
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+  console.log(req.body);
+  User.create(req.body, (error, createdUser) => {
+    if(error){
+      console.log(error);
+    } else {
+      console.log(createdUser);
+      res.redirect('/');
+    }
+  })
+})
+module.exports = router;
